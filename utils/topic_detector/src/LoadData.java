dc3bc022f67c0ad871cb98b07a6a5bf4a81d1446
Ubuntu <ubuntu@ip-10-218-55-227.ec2.internal>
2010-04-13 16:51:28 +0000

    updated db credentials

--- utils/topic_detector/src/LoadData.java
+++ utils/topic_detector/src/LoadData.java
@@ -6,7 +6,7 @@ public class LoadData{
 
         public static void main(String args[]) throws Exception{
 
-                WikipediaDatabase w = new WikipediaDatabase("localhost", "wikipedia", "wikiuser", "temp4now");
+                WikipediaDatabase w = new WikipediaDatabase("localhost", "wikipedia", "wikiuser", "z8efx");
                 w.loadData(new File(args[0]), true);
                 w.prepareForTextProcessor(new org.wikipedia.miner.util.text.CaseFolder());
 

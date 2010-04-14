5bd693ee3e10e852459ea7341d9df83dc5d5d4d5
Ubuntu <ubuntu@domU-12-31-39-0C-34-03.compute-1.internal>
2010-04-14 14:37:19 +0000

    fixing bugs for topic detector

---
+++ utils/topic_detector/src/Test.java
@@ -0,0 +1,11 @@
+import org.wikipedia.miner.model.WikipediaDatabase;
+import org.wikipedia.miner.model.*;
+import java.io.*;
+
+public class Test{
+
+	public static void main() throws Exception{
+		WikipediaDatabase w = new WikipediaDatabase("localhost", "wikipedia", "wikiuser", "z8efx");
+		// org.wikipedia.miner.model.WikipediaDatabase("localhost", "wikipedia", "wikiuser", "z8efx");
+	}
+}
